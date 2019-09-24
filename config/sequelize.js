c0d645c8a7c586668d4b5aa1d19d0def5dc1e5ab
Sergei <cergey@yandex.ru>
2019-09-24 21:56:57 +0700

    Bug fix and env example

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const databases = require('./main/mysql');
+const databases = require('./main/databases');
 
 const config = databases.guess_number;
 const env = process.env.NODE_ENV || 'development';
